a4877736be99ba9b5f6f61479936b491ea8e7ad5
Simon Letort <simon@artwaresolutions.org>
2014-03-02 13:39:05 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Artware2::Application.config.secret_key_base = 'd95e2a83e810dbf635e1402030298441381e50997f8710891e2cbb4fc82984f5585e61d63fa8c27ca13db62bb653ccaae6c6df7d138a969a9e80798738457f89'
